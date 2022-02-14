5c32b0c08156d47e6e96847a8c07cbaca40184fb
Denis <trumpk@gmail.com>
2022-02-14 17:17:59 +0300

    Django >4 version

--- robokassa/apps.py
+++ robokassa/apps.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 import django
-if django.VERSION[0] > 4:
+if django.VERSION[0] > 3:
     from django.utils.translation import gettext_lazy as _
 else:
     from django.utils.translation import ugettext_lazy as _
